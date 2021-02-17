62d7564aba09ee1d529669576bb9230d8a6e7acb
dimas sudjito <dimassudjito@localhost.localdomain>
2021-02-17 13:24:47 -0700

    set writing page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,6 +5,14 @@ const Header = () => {
         <div className="m-5">
             <h1 className="text-center text-white">CAMPFIRE</h1>
             <div><iframe src="https://giphy.com/embed/3ov9jHRnuHpAI77eHC" width="100%" height="100%" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="/write">Write a story</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="/acknowledgement">Acknowledgement</a>
+                </li>
+            </ul>
         </div>
     );
 };
--- client/src/components/Write.js
+++ client/src/components/Write.js
@@ -23,25 +23,27 @@ const Write = () => {
         <div className="m-5">
             <form onSubmit={postStory}>
                 <input 
-                    className="form-control m-3" 
+                    className="form-control m-3 bg-dark text-white" 
                     type="text" 
                     placeholder="Title"
                     value={title}
                     onChange={(event)=>{setTitle(event.target.value)}}
                 />
                 <textarea 
-                    className="form-control m-3" 
+                    className="form-control m-3 bg-dark text-white" 
                     placeholder="write your story here"
                     style={{height: "30em"}}
                     value={content}
                     onChange={(event)=>{setContent(event.target.value)}}
                 />
-                <button
-                    className="btn btn-primary mx-3"
-                    type="submit"
-                >
-                    Post
-                </button>
+                <div className="d-flex justify-content-end">
+                    <button
+                        className="btn btn-dark"
+                        type="submit"
+                    >
+                        Post
+                    </button>
+                </div>
             </form>
         </div>
     );
